d54526eba4786d12676035e9a72d20d55dad9f8c
Shalyat <121231433+Shalyat@users.noreply.github.com>
2023-05-16 15:00:35 +0700

    Update code.py

--- Level-1/code.py
+++ Level-1/code.py
@@ -1,16 +1,3 @@
-'''
-////////////////////////////////////////////////////////////
-///                                                      ///
-///   0. tests.py is passing but the code is vulnerable  /// 
-///   1. Review the code. Can you spot the bug?          ///
-///   2. Fix the code but ensure that tests.py passes    ///
-///   3. Run hack.py and if passing then CONGRATS!       ///
-///   4. If stuck then read the hint                     ///
-///   5. Compare your solution with solution.py          ///
-///                                                      ///
-////////////////////////////////////////////////////////////
-'''
-
 from collections import namedtuple
 
 Order = namedtuple('Order', 'id, items')
@@ -27,7 +14,7 @@ def validorder(order: Order):
         else:
             return("Invalid item type: %s" % item.type)
     
-    if net != 0:
+    if round(net, 2) != 0:
         return("Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net))
     else:
         return("Order ID: %s - Full payment received!" % order.id)
