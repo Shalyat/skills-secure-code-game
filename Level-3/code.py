9be90da4896825d8232a9d59e9b568cbaefa21da
Shalyat <121231433+Shalyat@users.noreply.github.com>
2023-05-16 15:22:31 +0700

    Update code.py

--- Level-3/code.py
+++ Level-3/code.py
@@ -1,14 +1,18 @@
 import os
 from flask import Flask, request  
 
-### Unrelated to the exercise -- Starts here -- Please ignore
+def safe_path(path):
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    filepath = os.path.normpath(os.path.join(base_dir, path))
+    if base_dir != os.path.commonpath([base_dir, filepath]):
+        return None
+    return filepath
+    
 app = Flask(__name__)
 @app.route("/")
 def source():
     TaxPayer('foo', 'bar').get_tax_form_attachment(request.args["input"])
     TaxPayer('foo', 'bar').get_prof_picture(request.args["input"])
-### Unrelated to the exercise -- Ends here -- Please ignore
-
 class TaxPayer:
     
     def __init__(self, username, password):
@@ -16,14 +20,11 @@ class TaxPayer:
         self.password = password
         self.prof_picture = None
         self.tax_form_attachment = None
-
-    # returns the path of an optional profile picture that users can set        
+   
     def get_prof_picture(self, path=None):
-        # setting a profile picture is optional
         if not path:
             pass
-        
-        # defends against path traversal attacks
+
         if path.startswith('/') or path.startswith('..'):
             return None
         
@@ -34,10 +35,8 @@ class TaxPayer:
         with open(prof_picture_path, 'rb') as pic:
             picture = bytearray(pic.read())
 
-        # assume that image is returned on screen after this
         return prof_picture_path
 
-    # returns the path of an attached tax form that every user should submit
     def get_tax_form_attachment(self, path=None):
         tax_data = None
         
@@ -47,5 +46,4 @@ class TaxPayer:
         with open(path, 'rb') as form:
             tax_data = bytearray(form.read())
 
-        # assume that taxa data is returned on screen after this
         return path
